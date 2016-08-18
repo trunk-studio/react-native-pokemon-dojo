1e9e7675907c0cc84643a6f657fb85c6c522b3d3
lyhcode <lyhcode@gmail.com>
2016-08-18 16:12:59 +0800

    update

--- src/containers/PokemonDetails.js
+++ src/containers/PokemonDetails.js
@@ -30,47 +30,32 @@ class PokemonDetails extends Component {
 
     const { pokemon } = this.props;
 
-    var info, moves, sprites, stats, ready;
-
-    ready = pokemon.get('sprites')?true:false;
-
-    if (ready) {
-      /* 提示：請取消註解這一段程式碼 */
-      /*
-      sprites = <Sprites sprites={ pokemon.get('sprites') } />
-
-      info = <BasicInfo
-              types  = { pokemon.get('types') }
-              weight = { pokemon.get('weight') }
-              height = { pokemon.get('height') } />
-
-      stats = <Stats stats={ pokemon.get('stats') } />
-
-      moves = <Moves moves={ pokemon.get('moves') } />
-      */
-    }
+    const basicInfo = {
+      types: pokemon.get('types'),
+      height: pokemon.get('height'),
+      weight: pokemon.get('weight'),
+    };
 
     return (
-      <Loader show={ !ready }>
+      <Loader show={ !pokemon.get('name') }>
 
         {/*
         步驟三、顯示神奇寶貝詳細資料
         */}
-        {/* 提示：請刪除下面這一段程式碼 */}
-        <View style={ styles.container }>
-          <Image source={{ uri: 'http://vignette2.wikia.nocookie.net/againstwar/images/0/0e/1062_body.png/revision/latest?cb=20140510025754&path-prefix=zh' }} style={{ width: 150, height: 150, margin: 15 }} />
-          <Text style={{ textAlign: 'center', alignSelf: 'stretch', color: '#ffffff', backgroundColor: '#FF0000' }}>第三關、解鎖神奇寶貝的神秘屬性</Text>
-          <Text style={{width: 250, lineHeight: 24, fontSize: 16, color: '#555555' }}>恭喜你已經快要完成練習！請嘗試讓神奇寶貝的各種屬性可以被正確顯示！</Text>
-        </View>
+        {/* 提示：請先刪除或註解下面這一行程式碼 */}
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: clrs.white }}><Image source={{ uri: 'http://vignette2.wikia.nocookie.net/againstwar/images/0/0e/1062_body.png/revision/latest?cb=20140510025754&path-prefix=zh' }} style={{ width: 150, height: 150, margin: 15 }} /><Text style={{ textAlign: 'center', alignSelf: 'stretch', color: '#ffffff', backgroundColor: '#FF0000' }}>第三關、解鎖神奇寶貝的神秘屬性</Text><Text style={{width: 250, lineHeight: 32, fontSize: 16, color: '#555555' }}>恭喜你已經快要完成練習！請嘗試讓神奇寶貝的各種屬性可以被正確顯示！</Text></View>
 
-        {/* 提示：請取消註解這一段程式碼 */}{/*{/*
+        {/* 提示：請取消註解下面這一段程式碼 */}{/*
         <ScrollView style={ styles.container }>
-          { sprites }
-          { info }
-          { stats }
-          { moves }
+          <Sprites sprites={ pokemon.get('sprites') } />
+          <BasicInfo { ...basicInfo } />
+          <Header>STATS</Header>
+          <Stats stats={ pokemon.get('stats') } />
+          <Header>MOVES</Header>
+          <Moves moves={ pokemon.get('moves') } />
         </ScrollView>
         */}
+
       </Loader>
     );
   }
@@ -104,7 +89,6 @@ function mapDispatchToProps(dispatch) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: clrs.white,
   },
 });
